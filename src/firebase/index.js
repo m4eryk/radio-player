78aeee66de62655c6eb53047f127254db77669d1
m4eryk <maerovwork@gmail.com>
2023-01-20 18:09:15 +0300

    add firebase config

---
+++ src/firebase/index.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCCWx_R0f4RC_q0SrOk8NRt2m6Y0PeVOwk",
+  authDomain: "radio-player-d2d84.firebaseapp.com",
+  projectId: "radio-player-d2d84",
+  storageBucket: "radio-player-d2d84.appspot.com",
+  messagingSenderId: "1078427788803",
+  appId: "1:1078427788803:web:4968aa70cac00ab1c15036",
+  measurementId: "G-VVSH8KQKF9"
+};
+
+export const app = initializeApp(firebaseConfig);
